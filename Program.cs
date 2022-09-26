9a5c6a68bce96348d9028f1a540173562303041e
Артём Шустов < @DESKTOP-3G11VV9>
2022-09-26 21:39:24 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//Task8
+
+int k = Convert.ToInt32(Console.ReadLine());
+int[] nums = new int[k];
+Random rnd = new Random();
+for (int i = 0; i < nums.Length; i++)
+{
+    nums[i] = rnd.Next() % 10;
+    Console.Write(nums[i]+" ");
+}
+Console.WriteLine('\n');
+int Poisk = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < nums.Length; i++)
+{
+    if (nums[i] == Poisk)
+    {
+        Console.WriteLine("Элемент вошёл " + (i + 1) + " по счёту");
+    }
+}
